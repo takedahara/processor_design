f852c0b53d4b0cfcdf549328ef8dfbd0c972c09e
STEKI CHUNG <stekichung@gmail.com>
2022-05-06 16:07:17 +0900

    add new version of 16-bit register

---
+++ register_16_new.v
@@ -0,0 +1,12 @@
+module register_16_new(reg_e, reg_write_en, reg_in, reg_out);
+	input           reg_e;
+    input           reg_write_en;
+	input	    [15:0] reg_in;
+	output	reg [15:0] reg_out;
+	
+	always @(posedge reg_e) begin
+		if(reg_write_en) begin // enabler
+            reg_out <= reg_in
+            end
+	
+endmodule
