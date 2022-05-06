eeb29d23e165a37e80545c43cc72259a1d8f85ce
Steki <63486231+stekichung@users.noreply.github.com>
2022-05-06 13:20:57 +0900

    Update register_16

--- register_16.v
+++ register_16.v
@@ -2,8 +2,7 @@ module register_16(reg_e, rst, reg_in, reg_out);
 	input 		reg_e;
 	input 		rst;
 	input	[15:0] reg_in;
-	output	[15:0] reg_out;
-	reg	[15:0] reg_out;
+	output	reg [15:0] reg_out;
 	
 	always @(posedge reg_e or negedge rst) begin
 		if(rst == 1'b0)
