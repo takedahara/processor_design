5dc8e0ea484f628fe0fd973628975223ec21ae0e
STEKI CHUNG <stekichung@gmail.com>
2022-05-09 20:46:00 +0900

    Replaced old register16 and control with new ones

--- register_16.v
+++ register_16.v
@@ -1,14 +1,12 @@
-module register_16(reg_e, rst, reg_in, reg_out);
-	input 		reg_e;
-	input 		rst;
-	input	[15:0] reg_in;
+module register_16_new(reg_e, reg_write_en, reg_in, reg_out);
+	input           reg_e;
+    input           reg_write_en;
+	input	    [15:0] reg_in;
 	output	reg [15:0] reg_out;
 	
-	always @(posedge reg_e or negedge rst) begin
-		if(rst == 1'b0)
-			reg_out	<= 16'b0000000000000000;
-		else
-			reg_out	<= reg_in;
-	end
+	always @(posedge reg_e) begin
+		if(reg_write_en) begin 
+            reg_out <= reg_in;
+            end
 	
 endmodule
