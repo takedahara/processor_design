40d492e952aa832134ca52f7864e5346a14663ac
takedahara <63288027+takedahara@users.noreply.github.com>
2022-05-20 13:43:36 +0900

    Add files via upload

--- program_counter.v
+++ program_counter.v
@@ -2,19 +2,22 @@ module program_counter(pc_e, rst,j_flag,j_addr,  pc_out);
 	input 		pc_e;
 	input 		rst;
 	input j_flag;
-	input[11:0]j_addr;
+	input[15:0]j_addr;
 	
 	
-	output	[11:0] pc_out;
+	output	[15:0] pc_out;
 	
-	reg	[11:0] pc_out=16'b0000000000000000;
+	reg	[15:0] pc_out;
 	
 	always @(posedge pc_e ) begin
 		if(j_flag==1)begin
-			pc_out<=j_addr;
+			pc_out<=j_addr+16'b0000000000000001; //j_addr ha PC+ext_d nanode +1 gahituyou
 		end
 		else begin
 		pc_out	<= pc_out+16'b0000000000000001;//pc_inwo16'b1nikaeta
+		end
+		if(rst==1)begin
+			pc_out<=16'b0000000000000000;
 		end
 	end
 	
